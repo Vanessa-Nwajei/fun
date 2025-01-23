d21b42579759ef0204cf197708f17d32604bb5cf
VanessaNwajei <vanicsam2005@gmail.com>
2025-01-23 10:45:56 +0100

    second

--- ins1.py
+++ ins1.py
@@ -1,17 +1,30 @@
-def ins(input_file):
-     with open(input_file, 'r') as f:
-        # First line is the number of elements
-        n = int(f.readline().strip())
-        # Second line contains the array elements
-        A= list(map(int, f.readline().split()))
-        swaps = 0
-        for i in range(1,len(A)):
-            k = i
-            while k > 0 and A[k]<A[k-1]:
-                A[k], A[k-1] = A[k-1], A[k]
-                k -= 1
-                swaps +=1
-        return swaps
-     
+def mer(file_path):
+    # Read input from text file
+    with open(file_path, 'r') as file:
+        # First line contains the first array
+        A = list(map(int, file.readline().strip().split()))
+        # Second line contains the second array
+        B = list(map(int, file.readline().strip().split()))
+    
+    # Merge procedure
+    result = []
+    i = j = 0
+    while i < len(A) and j < len(B):
+        if A[i] <= B[j]:
+            result.append(A[i])
+            i += 1
+        else:
+            result.append(B[j])
+            j += 1
+    
+    # Append remaining elements
+    result.extend(A[i:])
+    result.extend(B[j:])
+    
+    # Optional: write output to file
+    with open('output.txt', 'w') as outfile:
+        outfile.write(' '.join(map(str, result)))
+    
+    return result
 
-print(ins('rosalind_ins.txt'))
+print(mer('mer.txt'))
